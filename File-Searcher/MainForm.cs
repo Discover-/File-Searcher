446c49d743c988b71c4c288eb646898736308f95
Discover- <amort11@hotmail.com>
2013-08-28 08:41:25 +0200

    Use the full file path when trying to open a file from double click. Fixes issue where, if directory was not shown (checkbox unchecked), it would not succesfully open the file

--- File-Searcher/MainForm.cs
+++ File-Searcher/MainForm.cs
@@ -546,9 +546,9 @@ namespace File_Searcher
         {
             if (listViewResults.SelectedItems.Count == 1)
             {
-                string selectedItemName = listViewResults.SelectedItems[0].SubItems[1].Text;
+                string selectedItemName = listViewResults.SelectedItems[0].SubItems[5].Text;
 
-                if (!IsInvalidString(listViewResults.SelectedItems[0].SubItems[1].Text))
+                if (!IsInvalidString(listViewResults.SelectedItems[0].SubItems[5].Text))
                     if (MessageBox.Show("Are you sure you want to open this file?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                         StartProcess(selectedItemName);
             }
