Discover-/File-Searcher

commit 3e06651b752690674d4d90a42eed248537ca8de3

Move most (if not all?) calls that CAN be moved to the designer, to the designer

File-Searcher/FilteredResultsForm.cs
@@ -12,16 +12,8 @@ namespace File_Searcher
         public FilteredResultsForm(ListView.ListViewItemCollection items)
         {
             InitializeComponent();
-            MaximizeBox = false; //! This looks ugly if set to true when button is pressed while we have a hardcoded max size
-            MinimizeBox = true;
             MinimumSize = new Size(Width, Height);
             MaximumSize = new Size(Width, Height + 500);
-
-            initialItemCollection = items;
-            InitializeListView(ref listViewResultsFilter, items);
-
-            KeyPreview = true;
-            KeyDown += FiltedResultsForm_KeyDown;
         }
 
         private void button1_Click(object sender, EventArgs e)

File-Searcher/FilteredResultsForm.Designer.cs
@@ -85,9 +85,13 @@
             this.Controls.Add(this.comboBoxFilterType);
             this.Controls.Add(this.textBoxFilter);
             this.Controls.Add(this.listViewResultsFilter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
             this.Name = "FilteredResultsForm";
             this.Text = "Filtered results";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FiltedResultsForm_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
 

File-Searcher/MainForm.Designer.cs
@@ -134,11 +134,15 @@
             // 
             // listViewResults
             // 
+            this.listViewResults.FullRowSelect = true;
             this.listViewResults.Location = new System.Drawing.Point(14, 250);
             this.listViewResults.Name = "listViewResults";
             this.listViewResults.Size = new System.Drawing.Size(740, 240);
             this.listViewResults.TabIndex = 8;
             this.listViewResults.UseCompatibleStateImageBehavior = false;
+            this.listViewResults.View = System.Windows.Forms.View.Details;
+            this.listViewResults.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listViewResults_ColumnClick);
+            this.listViewResults.DoubleClick += new System.EventHandler(this.listViewResults_DoubleClick);
             // 
             // checkBoxShowDir
             // 
@@ -410,6 +414,7 @@
             this.menuItemSettings.Name = "menuItemSettings";
             this.menuItemSettings.Size = new System.Drawing.Size(116, 22);
             this.menuItemSettings.Text = "Settings";
+            this.menuItemSettings.Click += new System.EventHandler(this.menuItemSettings_Click);
             // 
             // toolStripSeparator1
             // 
@@ -421,6 +426,7 @@
             this.menuItemExit.Name = "menuItemExit";
             this.menuItemExit.Size = new System.Drawing.Size(116, 22);
             this.menuItemExit.Text = "Exit";
+            this.menuItemExit.Click += new System.EventHandler(this.TryCloseApplication);
             // 
             // menuHeaderAbout
             // 
@@ -435,6 +441,7 @@
             this.menuItemAbout.Name = "menuItemAbout";
             this.menuItemAbout.Size = new System.Drawing.Size(107, 22);
             this.menuItemAbout.Text = "About";
+            this.menuItemAbout.Click += new System.EventHandler(this.menuItemAbout_Click);
             // 
             // datePickerFilesNewerThan
             // 
@@ -496,10 +503,13 @@
             this.Controls.Add(this.checkBoxFilesOlderThan);
             this.Controls.Add(this.checkBoxFilesNewerThan);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "File Searcher";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.groupBoxOptions.ResumeLayout(false);
             this.groupBoxOptions.PerformLayout();
             this.groupBoxSearchInfo.ResumeLayout(false);

File-Searcher/MainForm.cs
@@ -29,8 +29,6 @@ namespace File_Searcher
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MaximizeBox = false; //! This looks ugly if set to true when button is pressed while we have a hardcoded max size
-            MinimizeBox = true;
             MinimumSize = new Size(Width, Height);
             MaximumSize = new Size(Width + 700, Height);
             //MaximumSize = new Size(Width + 700, Height + 50);
@@ -44,22 +42,12 @@ namespace File_Searcher
             timerMoveForDetailedRestrictions = new Timer { Enabled = false, Interval = 16 };
             timerMoveForDetailedRestrictions.Tick += timerMoveForDetailedRestrictions_Tick;
 
-            KeyPreview = true;
-            KeyDown += Form1_KeyDown;
-
-            listViewResults.View = View.Details;
             listViewResults.Columns.Add("Extension", 60, HorizontalAlignment.Right);
             listViewResults.Columns.Add("Name", 430, HorizontalAlignment.Left);
             listViewResults.Columns.Add("Size", 35, HorizontalAlignment.Right);
             listViewResults.Columns.Add("Sizetype", 55, HorizontalAlignment.Right);
             listViewResults.Columns.Add("Last Modified", 138, HorizontalAlignment.Right);
             listViewResults.Columns.Add("", 0, HorizontalAlignment.Right);
-
-            listViewResults.FullRowSelect = true; //! This will make clicking on a row in the results select the full row.
-
-            listViewResults.DoubleClick += listViewResults_DoubleClick;
-
-            listViewResults.ColumnClick += listViewResults_ColumnClick;
 
             //! Set all anchors; this makes the controls properly resize along with the form when it gets resized.
             InitializeAnchors();
@@ -87,10 +75,6 @@ namespace File_Searcher
 
             txtBoxDirectorySearch.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             txtBoxDirectorySearch.AutoCompleteSource = AutoCompleteSource.FileSystemDirectories;
-
-            menuItemExit.Click += TryCloseApplication;
-            menuItemSettings.Click += menuItemSettings_Click;
-            menuItemAbout.Click += menuItemAbout_Click;
 
             menuItemExit.ShortcutKeys = (Keys.Shift | Keys.F5);
             menuItemExit.ShortcutKeyDisplayString = "(Shift + F5)";

File-Searcher/SettingsForm.Designer.cs
@@ -236,9 +236,13 @@
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.buttonSave);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
             this.Name = "SettingsForm";
             this.Text = "File Searcher Settings";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SettingsForm_FormClosed);
             this.Load += new System.EventHandler(this.SettingsForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SettingsForm_KeyDown);
             this.tabControl1.ResumeLayout(false);
             this.tabPageGeneral.ResumeLayout(false);
             this.tabPageGeneral.PerformLayout();

File-Searcher/SettingsForm.cs
@@ -14,15 +14,6 @@ namespace File_Searcher
 
         private void SettingsForm_Load(object sender, EventArgs e)
         {
-            MaximizeBox = false;
-            MinimizeBox = true;
-            FormBorderStyle = FormBorderStyle.Fixed3D;
-
-            FormClosed += SettingsForm_FormClosed; //! To save settings
-
-            KeyPreview = true;
-            KeyDown += SettingsForm_KeyDown;
-
             checkBoxPromptOpenFile.Checked = Properties.Settings.Default.PromptOpenFile;
             checkBoxPromptShowProgressbar.Checked = Properties.Settings.Default.PromptShowProgressBar;
             checkBoxPromptToQuit.Checked = Properties.Settings.Default.PromptToQuit;

commit 5e853feeb94faa72db65e206c75a174415535954

Fix accidental revert in 3e06651b752690674d4d90a42eed248537ca8de3 which broke the filtered results form

File-Searcher/FilteredResultsForm.cs
@@ -12,8 +12,12 @@ namespace File_Searcher
         public FilteredResultsForm(ListView.ListViewItemCollection items)
         {
             InitializeComponent();
+
             MinimumSize = new Size(Width, Height);
             MaximumSize = new Size(Width, Height + 500);
+
+            initialItemCollection = items;
+            InitializeListView(ref listViewResultsFilter, items);
         }
 
         private void button1_Click(object sender, EventArgs e)