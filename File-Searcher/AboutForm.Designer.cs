39fc1e92c87a3d14d7e776be1e07b54f3704ef15
Discover- <amort11@hotmail.com>
2013-09-30 15:47:46 +0200

    Update copyright in the about form

--- File-Searcher/AboutForm.Designer.cs
+++ File-Searcher/AboutForm.Designer.cs
@@ -112,7 +112,7 @@
             this.labelCopyright.Name = "labelCopyright";
             this.labelCopyright.Size = new System.Drawing.Size(271, 17);
             this.labelCopyright.TabIndex = 21;
-            this.labelCopyright.Text = "Copyright todo";
+            this.labelCopyright.Text = "Copyright @ 2013 Discover-";
             this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelCompanyName
